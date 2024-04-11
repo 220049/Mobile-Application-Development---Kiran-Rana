6404b399872c3ac0f537358f2d117cd6ea893638
Shadip Kumar Joshi <220049@softwarica.edu.np>
2024-04-11 13:43:10 +0545

    classwork_1

---
+++ classwork_1/lib/1.dart
@@ -0,0 +1,31 @@
+// create a class name person in dart with properties fName, lName,age, address, phoneNumber and personId
+
+class Person {
+  //properties of class
+  String fName;
+  String lName;
+  int age;
+  String address;
+  int phoneNumber;
+  int personId;
+
+  Person(this.fName, this.lName, this.age, this.address, this.phoneNumber,
+      this.personId); // Constructor
+
+  // Method to display person details : function
+  void displayPerson() {
+    print('Name: $fName $lName');
+    print('Age: $age');
+    print('Address: $address');
+    print('Phone No.: $phoneNumber');
+    print('Person ID: $personId');
+  }
+}
+
+void main() {
+  // Creating an instance of Person
+  var person = Person('Ram', 'Shrestha', 18, 'Pipalbot', 9876554561, 4);
+
+  // Displaying person details
+  person.displayPerson();
+}
